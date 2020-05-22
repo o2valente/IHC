41ab228de9f308e274984c8978b3a4ec0e2875f7
Valente <pedro.valente@ua.pt>
2020-05-22 01:41:14 +0100

    added home page

--- Projeto_IHC/Projeto_IHC/Home.xaml.cs
+++ Projeto_IHC/Projeto_IHC/Home.xaml.cs
@@ -30,5 +30,25 @@ namespace Projeto_IHC
             Welcome welcomePage = new Welcome();
             this.NavigationService.Navigate(welcomePage);
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- Projeto_IHC/Projeto_IHC/obj/Debug/Projeto_IHC_Content.g.i.cs
+++ Projeto_IHC/Projeto_IHC/obj/Debug/Projeto_IHC_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/ihc_logo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/shopping_list.png")]
 
 
